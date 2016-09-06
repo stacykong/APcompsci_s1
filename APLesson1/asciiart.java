77239afe8f78a02af0e5f99bc1e2c8d0998a2e6d
stacykong <KongS9997@sduhsd.lan>
2016-09-06 08:13:10 -0700

    added files

---
+++ APLesson1/asciiart.java
@@ -0,0 +1,52 @@
+public class asciiart
+{
+	public static void main(String[]args)
+	{
+		System.out.println("(\\ /)");
+		System.out.println("( . .)");
+		System.out.println("(\") (\")");
+		System.out.println(" /\\___/\\");
+		System.out.println(" )     (");
+		System.out.println("=\\     /=");
+		System.out.println("  )   (");
+		System.out.println(" /     \\");
+		System.out.println(" )     (");
+		System.out.println("/       \\");
+		System.out.println("\\       /");
+		System.out.println(" \\__ __/");
+		System.out.println("    ))");
+		System.out.println("   //");
+		System.out.println("  ((");
+		System.out.println("   \\)");
+		System.out.println("                               /T /I");
+		System.out.println("                              / |/ | .-~/");
+		System.out.println("         /T               | \\I  |  I  Y.-~/");
+		System.out.println("        I l   /I       T\\ |  |  l  |  T  /");
+		System.out.println(" __  | \\l   \\l  \\I l __l  l   \\   `  _. |");
+		System.out.println(" \\ ~-l  `\\   `\\  \\  \\\\ ~\\  \\   `. .-~   |");
+		System.out.println("  \\   ~-. \"-.  `  \\  ^._ ^. \"-.  /  \\   |");
+		System.out.println(".--~-._  ~-  `  _  ~-_.-\"-.\" ._ /._ .\" ./");
+		System.out.println(" >--.  ~-.   ._  ~>-\"    \"\\\\   7   7   ]");
+		System.out.println("^.___~\"--._    ~-{  .-~ .  `\\ Y . /    |");
+		System.out.println(" <__ ~\"-.  ~       /_/   \\   \\I  Y   : |");
+		System.out.println("   ^-.__           ~(_/   \\   >._:   | l______");
+		System.out.println("       ^--.,___.-~\"  /_/   !  `-.~\"--l_ /     ~\"-.");
+		System.out.println("              (_/ .  ~(   /'     \"~\"--,Y   -=b-. _)");
+		System.out.println("               (_/ .  \\  :           / l      c\"~o \\");
+		System.out.println("                \\ /    `.    .     .^   \\_.-~\"~--.  )");
+		System.out.println("                 (_/ .   `  /     /       !       )/");
+		System.out.println("                  / / _.   '.   .':      /        \'");
+		System.out.println("                  ~(_/ .   /    _  `  .-<_");
+		System.out.println("                    /_/ . ' .-~\" `.  / \\  \\          ,z=.");
+		System.out.println("                    ~( /   '  :   | K   \"-.~-.______//");
+		System.out.println("                      \"-,.    l   I/ \\_    __{--->._(==.");
+		System.out.println("                       //(     \\  <    ~\"~\"     //");
+		System.out.println("                      /' /\\     \\  \\     ,v=.  ((");
+		System.out.println("                    .^. / /\\     \"  }__ //===-  `");
+		System.out.println("                   / / ' '  \"-.,__ {---(==-");
+		System.out.println("                 .^ '       :  T  ~\"   ll");
+		System.out.println("                / .  .  . : | :!        \\\\");
+		System.out.println("               (_/  /   | | j-\"          ~^");
+		System.out.println("                 ~-<_(_.^-~\"");
+	}
+}
---
+++ APLesson1/firstprog.java
@@ -0,0 +1,8 @@
+public class firstprog
+{
+	public static void main (String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+	}	
+}
