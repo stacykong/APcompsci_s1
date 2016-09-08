8b3c9cda60767fde0d26ab85d3389b2215023fa3
stacykong <KongS9997@sduhsd.lan>
2016-09-08 09:20:45 -0700

    edit

--- APLesson03/L3_ex3.java
+++ APLesson03/L3_ex3.java
@@ -5,7 +5,7 @@ public class L3_ex3
 	public static void main(String[]args)
 	{
 		Scanner calc = new Scanner(System.in);
-		System.out.println("Pick a number for the radius.");
+		System.out.println("The equation for circular motion is (m(v^2)/r) = F.\nPick a number for the radius.");
 		int r = calc.nextInt();
 		System.out.println("Pick a number for the velocity.");
 		int v = calc.nextInt();
