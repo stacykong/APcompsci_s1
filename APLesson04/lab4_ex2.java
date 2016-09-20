8c8ccb1e92833336be8217d77b21c2f5b1d938c5
stacykong <stacykong55724@gmail.com>
2016-09-20 09:16:59 -0700

    changes

--- APLesson04/lab4_ex2.java
+++ APLesson04/lab4_ex2.java
@@ -9,28 +9,29 @@ public class lab4_ex2
 		Scanner kb = new Scanner(System.in);
 		
 		System.out.println("Enter your first name:");
-		String fn = kb.next();
+		//String n1 = kb.nextLine();
+		String fn = kb.nextLine();
 		System.out.println("Enter your last name:");
-		String ln = kb.next();
+		String ln = kb.nextLine();
 		System.out.println("Enter your title:");
-		String t = kb.next();
+		String t = kb.nextLine();
 		System.out.println("Enter the school name:");
-		String nl = kb.nextLine();
+		//String nl = kb.nextLine();
 		String sch = kb.nextLine();
 		System.out.println("Enter the school year:");
 		String yr = kb.nextLine();
 		System.out.println("Enter your subject:");
 		String sub = kb.nextLine();
 		
-		System.out.println("******************************");
+		System.out.println("************************************");
 		form.format(sch, yr);
 		form.format(fn, ln);
 		form.format(t, sub);
-		System.out.println("******************************");
+		System.out.println("************************************");
 	}
 	
 	public void format(String s1, String s2)
 	{
-		System.out.printf("* %12s  %12s *\n", s1, s2);
+		System.out.printf("* %12s  %18s *\n", s1, s2);
 	}
 }
